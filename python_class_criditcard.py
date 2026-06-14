ee27e3d905eba176a2748b56575dde994cff0e44
ahradwani <myipodsound@gmail.com>
2026-06-14 21:56:12 +0300

    Create python_class_criditcard.py
    cridit card class created in python

---
+++ python_class_criditcard.py
@@ -0,0 +1,20 @@
+class CriditCard :
+    def __init__ (self, owner, balance):
+        self.owner = owner 
+        self.balance = balance
+
+    def cridit (self, amount) :
+        # To add money to the card 
+        self.balance += amount
+
+    def withdraw (self,amount) :
+        # First we check if the balance is more than the amount of withdraw
+        if selef.balance < amount :
+            print("You have No Balance to withdraw.!")
+        else:
+            self.balance -= amount
+            print(f"  Your new balance is {self.balance}.")
+
+    def __str__ (self):
+        return (f" The Customer {self} have {self.balane} in the acount.")
+            
